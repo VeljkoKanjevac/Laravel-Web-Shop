1d3823bf2e1638363d9af12ebcceb4184e72c5fd
VeljkoKanjevac <Kanjevacveljko@gmail.com>
2025-02-21 22:24:36 +0100

    *Added Route groups for admin and users

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\AdminCheckMiddleware;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//USER RUTE
+Route::middleware('auth')->group(function () {
+
+});
+
+
+//ADMIN RUTE
+Route::middleware(['auth', AdminCheckMiddleware::class])->prefix('/admin')->name('admin.')->group(function () {
+
+});
+
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
